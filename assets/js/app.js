d9d6d77c3967df12bb7e9cfd2ce57667a9873b4d
MarioBlasi <mblasi509@gmail.com>
2023-03-28 16:10:36 +0100

    class in h1 and id='app'

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
-      medium: "100px",
       image:
         "https://img1.wallspic.com/crops/1/3/8/3/6/163831/163831-cartoon-grogu-yoda-visual_arts-art-3300x4125.jpg",
     };
