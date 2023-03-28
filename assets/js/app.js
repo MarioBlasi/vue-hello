b276747f766e89ebb45bcd3c30cd7b30acdc7032
MarioBlasi <mblasi509@gmail.com>
2023-03-28 14:15:32 +0100

    img from data

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,9 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
-      image: "./assets/img/Grogu_Wallpaper.webp",
+      medium: "100px",
+      image:
+        "https://img1.wallspic.com/crops/1/3/8/3/6/163831/163831-cartoon-grogu-yoda-visual_arts-art-3300x4125.jpg",
     };
   },
 }).mount("#app");
